784536046f774d65d2a1e0c9068711887461bd55
hAPPY <51069823+gaoqihan@users.noreply.github.com>
2020-05-18 22:40:15 +0800

    Add files via upload

--- explore.py
+++ explore.py
@@ -2,7 +2,7 @@ from multi_rake import Rake
 import requests
 import time
 
-main()
+
 
 def get_relatedwords(word):
     resp = requests.get('http://api.conceptnet.io/related/c/en/%s?filter=/c/en'%(word))
@@ -35,17 +35,35 @@ def two_level_relate(word):
     print(len(final))
     print(final)
     return final
-def main():
-    title=input("Enter the course title:\n")
+def matching(keywords,webTitle):#return true if overlap exist,i.e. pass
+    web_key=getKey(webTitle)
+    web_key_list=[]
+    for i in web_key:
+        web_key_list=web_key_list+[i[0]]
+    isDisjoint = not set(web_key_list).isdisjoint(set(keywords))
+    return isDisjoint
+    
+def getKey(text):
     rake=Rake()
-    keywords=rake.apply(title)
-    print(keywords[:5])
-    final_out=[]
-    for i in keywords[:5]:
-        final_out=final_out+two_level_relate(i[0])
+    keywords=rake.apply(text)
+    print(keywords[:7])    
+    return keywords[:7]
+
+def main():
+    courseTitle=input("Enter the course title:\n")
+    webTitle="yes let's study some physics"
+    keywords=getKey(courseTitle)
+    courseTitle_list=[]
+    for i in keywords[:7]:
+        courseTitle_list=courseTitle_list+get_relatedwords(i[0])
+    decision=matching(courseTitle_list,webTitle)
+    if decision==True:
+        print("go ahead")
+    else:
+        print('focus!')
     return None
 
-
+main()
 #print(outp)
 #out=outp.split("}")
 #print(type(out))
